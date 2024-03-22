de348fc5d0a4971b7d0d7e8a192686a092f4ed12
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-03-22 11:03:42 +0100

    feat(cache): Fix hash collisions and use faster algo

--- src/cache.rs
+++ src/cache.rs
@@ -1,19 +1,23 @@
-use std::{
-    collections::hash_map::DefaultHasher,
-    hash::{Hash, Hasher},
-};
+use std::hash::{BuildHasher, Hash, Hasher};
 
 use crate::{render::Canvas, trim_transparency};
 use lru::LruCache;
 use tiny_skia::PixmapPaint;
 use usvgr::HashedNode;
 
+struct SvgrCacheInternal<HashBuilder: BuildHasher = ahash::RandomState> {
+    lru: LruCache<u64, FromPixmap>,
+    hash_builder: HashBuilder,
+}
+
 /// Defines rendering LRU cache. Each individual node and group will be cached separately.
 /// Make sure that in most cases it will require saving of the whole canvas which may lead to significant memory usage.
 /// So it is recommended to set the cache size to a reasonable value.
 ///
 /// Pass &mut SvgrCache::none() if you don't need caching.
-pub struct SvgrCache(Option<LruCache<u64, FromPixmap>>);
+pub struct SvgrCache<RandomState: BuildHasher = ahash::RandomState>(
+    Option<SvgrCacheInternal<RandomState>>,
+);
 
 pub struct FromPixmap {
     pub pixmap: tiny_skia::Pixmap,
@@ -41,6 +45,15 @@ impl FromPixmap {
 }
 
 impl SvgrCache {
+    /// Creates a new cache with the specified capacity.
+    /// If capacity <= 0 then cache is disabled and this struct does not allocate.
+    /// Uses `ahash` as a hasher, if you want to specify custom hasher user `new_with_hasher` fn.
+    pub fn new(size: usize) -> Self {
+        Self::new_with_hasher(size, ahash::RandomState::default())
+    }
+}
+
+impl<THashBuilder: BuildHasher + Default> SvgrCache<THashBuilder> {
     /// Creates a no cache value. Basically an Option::None.
     pub fn none() -> Self {
         Self(None)
@@ -48,11 +61,12 @@ impl SvgrCache {
 
     /// Creates a new cache with the specified capacity.
     /// If capacity <= 0 then cache is disabled.
-    pub fn new(size: usize) -> Self {
+    pub fn new_with_hasher(size: usize, hasher: THashBuilder) -> Self {
         if size > 0 {
-            Self(Some(LruCache::new(
-                std::num::NonZeroUsize::new(size).unwrap(),
-            )))
+            Self(Some(SvgrCacheInternal {
+                lru: LruCache::new(std::num::NonZeroUsize::new(size).unwrap()),
+                hash_builder: hasher,
+            }))
         } else {
             Self::empty()
         }
@@ -63,21 +77,27 @@ impl SvgrCache {
         Self(None)
     }
 
-    fn hash(&self, node: &usvgr::Node) -> u64 {
-        let mut hasher = DefaultHasher::new();
+    fn hash(&self, node: &usvgr::Node) -> Option<u64> {
+        let cache = self.0.as_ref()?;
+
+        let mut hasher = cache.hash_builder.build_hasher();
         HashedNode(node).hash(&mut hasher);
-        hasher.finish()
+        Some(Hasher::finish(&hasher))
     }
 
-    /// Creates sub pixmap that will be cached itself withing a canvas cache. Guarantees empty canvas within closure.  
+    /// Creates sub pixmap that will be cached itself within a canvas cache. Guarantees empty canvas within closure.  
     pub(crate) fn with_subpixmap_cache(
         &mut self,
         node: &usvgr::Node,
         canvas: &mut Canvas,
-        mut f: impl FnMut(&mut Canvas, &mut SvgrCache) -> FromPixmap,
+        mut f: impl FnMut(&mut Canvas, &mut SvgrCache<THashBuilder>) -> FromPixmap,
     ) {
         let hash = self.hash(node);
-        let cached_value = self.0.as_mut().and_then(|cache| cache.get(&hash));
+        let cached_value = self
+            .0
+            .as_mut()
+            .zip(hash)
+            .and_then(|(cache, hash)| cache.lru.get(&hash));
 
         if let Some(cached_value) = cached_value {
             cached_value.draw_into(canvas)
@@ -96,8 +116,8 @@ impl SvgrCache {
             let value = f(&mut temp_canvas, self);
             value.draw_into(canvas);
 
-            if let Some(cache) = self.0.as_mut() {
-                cache.put(hash, value);
+            if let Some((cache, hash)) = self.0.as_mut().zip(hash) {
+                cache.lru.put(hash, value);
             }
         };
     }
@@ -106,7 +126,7 @@ impl SvgrCache {
         &mut self,
         canvas: &mut Canvas,
         node: &usvgr::Node,
-        mut f: impl FnMut(&mut Canvas, &mut SvgrCache),
+        mut f: impl FnMut(&mut Canvas, &mut SvgrCache<THashBuilder>),
     ) {
         if canvas.skip_caching {
             f(canvas, self);
@@ -114,7 +134,11 @@ impl SvgrCache {
         }
 
         let hash = self.hash(node);
-        let cached_value = self.0.as_mut().and_then(|cache| cache.get(&hash));
+        let cached_value = self
+            .0
+            .as_mut()
+            .zip(hash)
+            .and_then(|(cache, hash)| cache.lru.get(&hash));
 
         if let Some(cached_value) = cached_value {
             cached_value.draw_into(canvas)
@@ -152,8 +176,8 @@ impl SvgrCache {
 
             value.draw_into(canvas);
 
-            if let Some(cache) = self.0.as_mut() {
-                cache.put(hash, value);
+            if let Some((cache, hash)) = self.0.as_mut().zip(hash) {
+                cache.lru.put(hash, value);
             }
         };
     }
